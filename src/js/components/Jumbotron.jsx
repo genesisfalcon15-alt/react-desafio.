85fb77c0287f924aa9509ee0df57b96bfa9f9966
Jonathan C <jcastanedachavarro@gmail.com>
2026-02-08 18:57:54 +0000

    aprendiendo-react>

---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,13 @@
+function Jumbotron () {
+        return(
+            <div className="card text-bg-light w-75 mx-auto p-2"> 
+             <div className="card-body">
+                <h1 className="card-title">A Warm Welcome!</h1>
+                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, iste porro inventore voluptatibus veniam, excepturi recusandae dolor aperiam deleniti quisquam sed labore quia? Culpa animi illo, ex distinctio amet explicabo.</p>
+                <a href="#" className="btn btn-primary">Call to action!</a>
+             </div>
+            </div>
+        );
+}
+
+export default Jumbotron;
